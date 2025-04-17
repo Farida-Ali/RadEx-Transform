99b660030ffef09d77bf7c4f2cee2aba1d121d0d
Farida Ali Mohsen <85225423+Farida-Ali@users.noreply.github.com>
2025-04-17 13:53:41 +0300

    Update demo_radex.py

--- examples/demo_radex.py
+++ examples/demo_radex.py
@@ -2,7 +2,7 @@ from radex.radex import generate_radex_image
 import matplotlib.pyplot as plt
 
 # Provide the path to your image clearly
-image_path = 'sample_image.png'  
+image_path = 'test1.png'  
 
 # Generate RadEx transformed image
 radex_img = generate_radex_image(image_path)
